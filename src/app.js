a928ad1f805616e5098441aab4de2246e9870cb6
Tony Le <lvtien.dev@gmail.com>
2013-11-02 15:22:35 +0800

    Port 80 experiment

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ var io = require('socket.io');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());

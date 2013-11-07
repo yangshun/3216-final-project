d72feeee4dc0be6322637a823ecb33413d3c5e70
Yang Shun <tay.yang.shun@gmail.com>
2013-11-07 20:29:24 +0800

    added game message bar

--- src/public/js/arena/leaderboard-controller.js
+++ src/public/js/arena/leaderboard-controller.js
@@ -10,23 +10,25 @@ function LeaderboardController($scope) {
   $scope.addPlayer = function(msg, data) {
     var person = {'name': data.name, 'kills': 0, 'deaths': 0};
     $scope.player_list.push(person);
+    $scope.$parent.game_message = data.name + ' has joined the game!';
     $scope.$apply();
   };
   
   $scope.updatePlayer = function(msg, data) {
+    console.log(data);
     var killer =_.findWhere($scope.player_list, {name : data.killer});
     killer.kills += 1;
     var victim = _.findWhere($scope.player_list, {name : data.victim});
     victim.deaths += 1;
-
+    $scope.$parent.game_message = data.killer + ' just killed ' + data.victim + '!';
     $scope.$apply();
-    return killer && victim
   };
 
   $scope.removePlayer = function(msg, data) {
     $scope.player_list = _.filter($scope.player_list, function(p) {
       return p.name != data.name;
     });
+    $scope.$parent.game_message = data.name + ' has left the game.';
     $scope.$apply();
   }
 

2b5d0576423e6d2a79d4b1e59f5232535dd868f4
Yang Shun <tay.yang.shun@gmail.com>
2013-11-08 11:48:02 +0800

    add sfx for all shurikens

--- src/public/js/arena/sound.js
+++ src/public/js/arena/sound.js
@@ -1,6 +1,12 @@
 SoundManager = {
   sounds : [{id: "shooting-sound-1", src: "/sound/shooting-sound-1.mp3"},
             {id: "shooting-sound-2", src: "/sound/shooting-sound-2.mp3"},
+            {id: "banana-sound", src: "/sound/banana.wav"},
+            {id: "frost-sound", src: "/sound/frost.mp3"},
+            {id: "laser-sound", src: "/sound/laser.wav"},
+            {id: "missile-sound", src: "/sound/missile.mp3"},
+            {id: "plasma-sound", src: "/sound/plasma.mp3"},
+            {id: "punch-sound", src: "/sound/punch.mp3"},
             {id: "bgm", src: "/sound/ROAM - Hold The Fort.mp3"}],
   play: function(id, opts) {
     opts = opts || {};
@@ -11,4 +17,28 @@ SoundManager = {
 
 PubSub.subscribe('shuriken.shuriken.shoot', function(data) {
   SoundManager.play(Math.random() > 0.5 ? 'shooting-sound-1' : 'shooting-sound-2');
+});
+
+PubSub.subscribe('shuriken.banana.shoot', function(data) {
+  SoundManager.play('banana-sound');
+});
+
+PubSub.subscribe('shuriken.hulk_fist.shoot', function(data) {
+  SoundManager.play('punch-sound');
+});
+
+PubSub.subscribe('shuriken.laser.shoot', function(data) {
+  SoundManager.play('laser-sound');
+});
+
+PubSub.subscribe('shuriken.plasma.shoot', function(data) {
+  SoundManager.play('plasma-sound');
+});
+
+PubSub.subscribe('shuriken.rocket.shoot', function(data) {
+  SoundManager.play('missile-sound');
+});
+
+PubSub.subscribe('shuriken.snowflake.shoot', function(data) {
+  SoundManager.play('frost-sound');
 });

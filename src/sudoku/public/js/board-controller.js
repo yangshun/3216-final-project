d4b34fa91c2ef696421472cc93f85ebe742b8430
Yang Shun <tay.yang.shun@gmail.com>
2013-11-10 21:13:30 +0800

    remove redundant code

--- src/sudoku/public/js/board-controller.js
+++ src/sudoku/public/js/board-controller.js
@@ -68,16 +68,8 @@ function BoardController($scope, $timeout) {
   
   $scope.logs = [];
 
-  var effect = 'shake';
-  $scope.apple_image_class = {'brand-logo': true, 'animated': true, effect: false };
-  $scope.apple_count = 0;
-  $scope.android_image_class = {'brand-logo': true, 'animated': true, effect: false };
-  $scope.android_count = 0;
-
   socket.on('welcome', function(data) {
-    // $scope.apple_count = data.apple_count;
-    // $scope.android_count = data.android_count;
-    // $scope.$apply();
+
   });
 
   socket.on('update-board', function(data) {
@@ -102,22 +94,4 @@ function BoardController($scope, $timeout) {
     return ref[data.col-1] + data.row;
   }
 
-  $scope.update_count = function(type) {
-    console.log('update count for', type);
-    switch (type) {
-      case 'apple':
-        $scope.apple_image_class[effect] = true;
-        $timeout(function() {
-          $scope.apple_image_class[effect] = false;
-        }, 500);
-        break;
-      case 'android':
-        $scope.android_image_class[effect] = true;
-        $timeout(function() {
-          $scope.android_image_class[effect] = false;
-        }, 500);
-        break;
-    }
-    $scope.$apply();
-  };
 }

ccfeba2725966d526f7ab057108ab43899929267
Tony Le <lvtien.dev@gmail.com>
2013-11-20 11:26:00 +0800

    Change shakeoff instance

--- src/shakeoff_una/deploytools/fabfile.py
+++ src/shakeoff_una/deploytools/fabfile.py
@@ -10,7 +10,7 @@ from fabric.api import *
 from fabric.colors import *
 
 PROJECT_PATH = '/home/ubuntu/2013-final-project-3/src/shakeoff_una'
-AWS_IP = '54.254.204.98'
+AWS_IP = '54.254.205.17'
 SERVER = [AWS_IP]
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/gsync.pem'
